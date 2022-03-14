edc88d39c28d857ffa3fdaabf897970c6320a4a0
Jasar Mohsin <94883109+jasuxks@users.noreply.github.com>
2022-03-14 17:26:28 +0000

    p1final

--- p1final.c
+++ p1final.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <math.h>
+void input(float *x1,float *x2,float *y1,float *y2)
+{
+	printf("enter values");
+	scanf("%f %f %f %f",x1,x2,y1,y2);
+}
+void distance(float x1,float x2,float y1,float y2,float *area)
+{
+	float a=(x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
+	*area= sqrt(a);
+}
+void output(float x1,float x2,float y1,float y2,float area)
+{
+	printf("distance between the points are %f",area);
+}
+int main()
+{
+	float x1,x2, y1,y2,area;
+	input(&x1,&x2,&y1,&y2);
+	distance(x1,x2,y1,y2,&area);
+	output(x1,x2,y1,y2,area);
+	return 0;
+}
--- p8final.c
+++ p8final.c
@@ -1,1 +1,72 @@
+#include<stdio.h>
+struct _triangle
+{
+float base,altitude,area;
+};
+typedef struct _triangle Triangle;
+int input_n()
+{
+	int n;
+	printf("how many triangles to analyzed:");
+	scanf("%d",&n);
+	return n;
+}
+Triangle input_triangle()
+{
+	Triangle input;
+	printf("enter the altitude and base of the triangle:");
+	scanf("%f %f",&input.altitude,&input.base);
+	input.area=0;
+	return input;
+}
+void input_n_triangles(int n, Triangle t[n])
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		t[i]=input_triangle();
+	}
+}
+void find_area(Triangle *t)
+{
+	t->area=0.5*t->altitude*t->area;
+}
+void find_areas_n(int n, Triangle t[n])
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		find_area(&t[i]);
+	}
+}
+Triangle find_smallest_triangle(int n, Triangle t[n])
+{
+	int j,i=0,count=0;
+	for(j=0;j<n;j++)
+	{
+		if((t[i].area)<=(t[j].area))
+		{
+			count=i;	
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return t[count];
+}
+void output(int n, Triangle t[n], Triangle smallest)
+{
+ printf("the smallest triangle with heigth %.1f and base %.1f has area of %.1f",smallest.altitude,smallest.base,smallest.area);
 
+}
+void main()
+{
+	int n;
+	n=input_n();
+	Triangle t[n];
+	input_n_triangles(n,&t[n]);
+	find_areas_n(n,&t[n]);
+	Triangle smallest=find_smallest_triangle(n,&t[n]);
+	output(n,&t[n],smallest);	
+}
